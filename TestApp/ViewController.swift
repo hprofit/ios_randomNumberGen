585bcecf8b7c3d0a02394363e7dc0985137fb5d8
holden_profit <holdenprofit@gmail.com>
2016-11-11 22:28:26 -0500

    Added random number gen.

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lowerBoundField: UITextField!
+    @IBOutlet var upperBoundField: UITextField!
+    
+    @IBOutlet var displayNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //lowerBoundField.text = "Hello"
+        //upperBoundField.text = "World!"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func generateUInt32BetweenBounds(lowerBound: UInt32, upperBound: UInt32) -> UInt32 {
+        let upperBoundBumped: UInt32 = upperBound + 1
+        let difference: UInt32 = upperBoundBumped - lowerBound
+        return UInt32(arc4random_uniform(upperBoundBumped - difference)) + lowerBound
+    }
+    
+    
+    @IBAction func generateRandomNumber(){
+        guard lowerBoundField.text != nil && upperBoundField.text != nil else {
+            return
+        }
+        let lowerBound: UInt32 = UInt32(lowerBoundField.text!)!
+        let upperBound: UInt32 = UInt32(upperBoundField.text!)!
+        
+        displayNumberLabel.text = String(generateUInt32BetweenBounds(lowerBound: lowerBound, upperBound: upperBound))
+    }
 
 }
 
